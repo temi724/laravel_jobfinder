51186a71d193c21c8a078e9a27cdd91cc3e02e0c
DANIEL TEMITAYO PETER <tayodaniel0@gmail.com>
2025-09-15 16:07:04 +0100

    Changes to be committed: 	new file:   job-finder/resources/views/components/job-card.blade.php 	modified:   job-finder/resources/views/jobs/index.blade.php 	modified:   job-finder/resources/views/jobs/show.blade.php

--- job-finder/resources/views/jobs/index.blade.php
+++ job-finder/resources/views/jobs/index.blade.php
@@ -1,35 +1,12 @@
 <x-layout>
     <div class="div">
         @foreach ($jobs as $job)
-            <x-card class="mb-4">
-                <div class="flex  justify-between">
-                    <div>
-                        {{ $job->title }}
-                    </div>
-                    <div class="text-sm">
-                        ₦{{ number_format($job->salary) }}
-                    </div>
-                </div>
-                <div class="flex text-xs justify-between items-center mb-4">
-
-                    <div class="flex space-x-2">
-                        <p>Company:</p>
-
-                        {{ $job->company }}
-                    </div>
-                    <div class="flex space-x-2">
-                        <div class="border p-1 rounded-md">{{ $job->category }}</div>
-                        <div class="border p-1 rounded-md"> {{ $job->experience }}</div>
-
-                    </div>
-                </div>
-                {{-- <p class="text-sm">{!!Str::limit($job->description, 300)!!}</p> --}}
-                <p class="text-sm mb-4""> {!! nl2br(e($job->description)) !!}</p>
+            <x-job-card class="mb-4" :$job>
                 <div>
                     <a href="{{ route('jobs.show', $job) }}"
                         class="text-white text-sm bg-gray-400 px-4 py-2 rounded-md hover:bg-black">View Details</a>
                 </div>
-            </x-card>
+            </x-job-card>
         @endforeach
     </div>
 </x-layout>
--- job-finder/resources/views/jobs/show.blade.php
+++ job-finder/resources/views/jobs/show.blade.php
@@ -1,29 +1,6 @@
 <x-layout>
-        <x-card class="mb-4">
-                <div class="flex  justify-between">
-                    <div>
-                        {{ $job->title }}
-                    </div>
-                    <div class="text-sm">
-                        ₦{{ number_format($job->salary) }}
-                    </div>
-                </div>
-                <div class="flex text-xs justify-between items-center mb-4">
+    <x-job-card class="mb-4" :$job>
 
-                    <div class="flex space-x-2">
-                        <p>Company:</p>
-
-                        {{ $job->company }}
-                    </div>
-                    <div class="flex space-x-2">
-                        <div class="border p-1 rounded-md">{{ $job->category }}</div>
-                        <div class="border p-1 rounded-md"> {{ $job->experience }}</div>
-
-                    </div>
-                </div>
-                {{-- <p class="text-sm">{!!Str::limit($job->description, 300)!!}</p> --}}
-                <p class="text-sm mb-4""> {!! nl2br(e($job->description)) !!}</p>
-
-            </x-card>
+    </x-job-card>
 
 </x-layout>
